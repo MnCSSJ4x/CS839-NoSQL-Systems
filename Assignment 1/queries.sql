d61b8dcb0543601fb282ad516b177b2be669584d
MnCSSJ4x <Monjoy.Choudhury@iiitb.ac.in>
2023-01-30 23:21:41 +0530

    add : problem 6

--- Assignment 1/queries.sql
+++ Assignment 1/queries.sql
@@ -44,7 +44,7 @@ SELECT uri as url from url where url.id in (SELECT temp3.article_id from
 GROUP BY(article_id)) as temp where temp.count>0) and (term = 'infantri' or term = 'reinforc' or term = 'brigad' or term = 'fire'))) as 
 temp2 GROUP BY(temp2.article_id) ORDER BY sum DESC) as temp3);
 
-/* Problem 6 */ 
+/* Problem 6 -> Assumption 1: */ 
 SELECT uri as url from url where url.id in 
 (SELECT temp10.article_id from (SELECT temp7.article_id, greatest(temp7.scoreone, temp7.scorefour) AS maxx from 
 (SELECT temp9.article_id, temp8.scorefour, temp9.scoreone from ((SELECT temp2.article_id, sum(temp2.score) as "scorefour" from 
@@ -63,6 +63,18 @@ as temp4 where article_id not in
 (term = 'brigad' or term = 'infantri' or term = 'fire') GROUP BY(article_id)) as temp6 where temp6.count=3)) and 
 (term = 'reinforc'))) as temp4 GROUP BY(article_id)) as temp9 on temp8.article_id = temp9.article_id)) 
 as temp7 ORDER BY maxx DESC) as temp10);
+
+/* Problem 6 -> Assumption 2: */
+SELECT uri from url INNER JOIN
+(SELECT *,  (temp1.sumone + temp2.sumfour) as f_score from 
+(SELECT article_id as id1, sum(score) as sumone  from keywords where term = 'reinforc'  GROUP BY(id1)) as temp1 
+LEFT JOIN 
+(SELECT article_id as id2, count(article_id) as count, sum(score) as sumfour from keywords where 
+term = 'brigad' OR term = 'fire'  OR term = 'infantri' GROUP BY (id2)) as temp2 ON temp1.id1 = temp2.id2
+where temp2.count IS NULL OR temp2.count != 3
+)as comparision
+ON url.id = comparision.id1
+ORDER BY comparision.f_score DESC, comparision.sumone DESC;
 
 
 
